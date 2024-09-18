5e6c004c510d9bf0450211502bc56dcf148cb293
moz-sec <m0253c@gmail.com>
2024-09-18 15:29:25 +0900

    feat: add install script

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 # dotfiles
 
-Set up Mac or Linux.
-
-Keep the configuration file in this directory and put a symbolic link to dotfiles on the home directory.
+This repository contains configuration files.
 
 ## Setup
 
 ```bash
 git clone git@github.com:moz-sec/dotfiles.git
 cd dotfiles
+./install.sh
 ```
 
-You can also retrieve setup.sh from the Internet and run it as is, as follows.
+<!-- You can also retrieve setup.sh from the Internet and run it as is, as follows. -->
 
-```bash
-curl -o - https://raw.githubusercontent.com/moz-sec/dotfiles/main/setup.sh | bash
-```
+<!-- ```bash
+curl -o - https://raw.githubusercontent.com/moz-sec/dotfiles/main/install.sh | bash
+``` -->
 
 ## Configuration File
 
---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -eu
+
+helpmsg() {
+  echo "Usage: $0 [--help | -h]" 0>&2
+  echo ""
+}
+
+link_to_homedir() {
+  echo "backup old dotfiles..."
+  if [ ! -d "$HOME/.dotbackup" ];then
+    echo "$HOME/.dotbackup not found. Auto Make it"
+    mkdir "$HOME/.dotbackup"
+  fi
+
+  local script_dir
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+  local dotfiles_dir
+  dotfiles_dir="$(dirname ${script_dir})"
+  if [[ "$HOME" != "$dotfiles_dir" ]];then
+    for hidden_file in "$dotfiles_dir"/.??*; do
+      [[ $(basename "$hidden_file") == ".git" ]] && continue
+      # Delete symbolic links if they exist.
+      # If just a file exists, back it up.
+      if [[ -L "$HOME/$(basename "$hidden_file")" ]];then
+        rm -f "$HOME/$(basename "$hidden_file")"
+      elif [[ -e "$HOME/$(basename "$hidden_file")" ]];then
+        mv "$HOME/$(basename "$hidden_file")" "$HOME/.dotbackup"
+      fi
+      ln -snf "$hidden_file" "$HOME"
+    done
+  else
+    echo "same install src dest"
+  fi
+}
+
+while [ $# -gt 0 ];do
+  case ${1} in
+    --debug|-d)
+      set -uex
+      ;;
+    --help|-h)
+      helpmsg
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
+
+link_to_homedir
+
+echo -e '\033[1;32mInstall completed!!!!\033[0;39m'
